d0c26c20921b8468ead37227f84f17b7228c6d61
DAP <parra.diego@correounivalle.edu.co>
2018-10-04 16:04:39 -0500

    Estado en su forma mas simple

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.changeTitle=this.changeTitle.bind(this)
+
+    this.state = {
+      title: "Hola mundo desde estado"
+    }
+  }
+  
   render() {
     return (
-      <h1>Hola mundo12wqe3</h1>
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick = {this.changeTitle}>Cambie el titulo</button>
+      </div>
     );
+  }
+
+  changeTitle(){
+    this.setState({
+      title:"NuevoTitulo"
+    })
   }
 }
 
