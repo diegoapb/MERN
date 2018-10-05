2cd7a2091eccc884a54a55c198b82b3725eafd40
DAP <parra.diego@correounivalle.edu.co>
2018-10-05 14:04:23 -0500

    formulario sencillo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state ={
+      name:"",
+      terms:true
+    }
+  }
+
   render() {
     return (
-      <h1>Hola mundo12wqe3</h1>
+      <div>
+        <h1>Hola Mundo</h1>
+        <input type="text" value={this.state.name} onChange={this.updateName.bind(this)} />
+        <div>
+          <label>
+            <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)}/>Acepto los terminos
+          </label>
+        </div>
+        <button onClick={this.sayHi.bind(this)}>Say Hello!</button>
+      </div>
     );
+  }
+
+
+  updateTerms(event){
+    this.setState({
+      terms:event.target.checked
+    });
+  }
+
+  sayHi(){
+    if(this.state.terms){
+      alert('Hola '+this.state.name);  
+    } else{
+      alert('debes aceptar los terminos');
+    }
+    
+    
+  }
+
+  updateName(event){
+    this.setState({
+      name: event.target.value
+    });
   }
 }
 
