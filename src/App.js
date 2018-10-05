a19ea0cf4479ebbd41ba62807b658156d1be8517
DAP <parra.diego@correounivalle.edu.co>
2018-10-05 12:59:28 -0500

    add, update tasks.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,46 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    
+
+    this.state = {
+      tasks: ["task1","task2","task3","task4","task5"]
+    }
+  }
+  
   render() {
     return (
-      <h1>Hola mundo12wqe3</h1>
+      
+      <div>
+        <ul>
+          {this.state.tasks.map(task=>
+            <li>{task}</li>)}
+        </ul>
+        <h1>{this.state.tasks}</h1>
+        <button onClick = {this.addTask.bind(this)}>Add Task</button>
+        <button onClick = {this.updateTask.bind(this)}>Update Task</button>
+      </div>
+    );
+  }
+
+  addTask(){
+    this.setState({
+      tasks:this.state.tasks.concat("new task")
+    })
+  }
+
+  updateTask(){
+    const index=2;
+
+    const tasks=this.state.tasks;
+    this.setState(
+      {tasks:[
+        ...tasks.slice(0,index),
+        "actualizado",
+        ...tasks.slice(index+1)
+      ]}
     );
   }
 }
